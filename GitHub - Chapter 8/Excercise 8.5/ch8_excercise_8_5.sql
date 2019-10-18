304c68323225d3a5b948b15dc67c04dbdd7f708a
Daniël van Eeden <git@myname.nl>
2019-10-18 08:27:39 +0200

    Chapter 8 excercises, initial commit

---
+++ GitHub - Chapter 8/Excercise 8.5/ch8_excercise_8_5.sql
@@ -0,0 +1,10 @@
+DESCRIBE demo1;
+TRUNCATE TABLE demo1;
+INSERT INTO demo1 VALUES
+(1, 'first test'),
+(2, 'second test'),
+(3, 'third test');
+INSERT INTO demo1 VALEUS(
+(4, 'fourth test'),
+(1, 'second test');
+SELECT * FROM demo1;
