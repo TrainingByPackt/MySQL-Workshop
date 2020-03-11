dc6042dbd49dc36f3f346e1196e1baf3a7a44ead
Daniël van Eeden <git@myname.nl>
2020-03-11 07:29:43 +0100

    Update ch12

--- Chapter12/ch12_excercise_12_03.sql
+++ Chapter12/ch12_excercise_12_03.sql
@@ -5,7 +5,7 @@ DROP ROLE IF EXISTS 'appdeveloper';
 DROP USER IF EXISTS 'linda'@'%';
 DROP USER IF EXISTS 'john'@'%';
 DROP USER IF EXISTS 'vladimir'@'%';
-DROP USER IF EXISTS 'jack'@'%';
+DROP USER IF EXISTS 'victoria'@'%';
 --enable_warnings
 
 CREATE ROLE 'manager';
@@ -18,7 +18,7 @@ GRANT ALL ON mobileapp.* TO 'appdeveloper';
 CREATE USER 'linda'@'%' IDENTIFIED BY 'AkFernyeisjegs' DEFAULT ROLE manager;
 CREATE USER 'john'@'%' IDENTIFIED BY 'owvurewJatkinyegod' DEFAULT ROLE manager;
 CREATE USER 'vladimir'@'%' IDENTIFIED BY 'rusvawfyoaw' DEFAULT ROLE appdeveloper;
-CREATE USER 'jack'@'%' IDENTIFIED BY 'joigowInladdIc6' DEFAULT ROLE webdeveloper;
+CREATE USER 'victoria'@'%' IDENTIFIED BY 'joigowInladdIc6' DEFAULT ROLE webdeveloper;
 
 GRANT manager, webdeveloper, appdeveloper TO 'patrick'@'%';
 ALTER USER 'patrick'@'%' DEFAULT ROLE manager;
